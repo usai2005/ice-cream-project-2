48bde8a62d0d8385e7e8dfaf8de7a3d283121fcc
Victoria Fedkova <victoria.fedkova@gmail.com>
2022-12-28 18:05:14 +0200

    add js fixes
    добавляет закрытие модалки по Esc
    Фиксит мобильный вид Героя

--- src/js/modal-windows.js
+++ src/js/modal-windows.js
@@ -27,7 +27,7 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   /* Записываем в переменные массив элементов-кнопок и подложку.
-      Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
+       Подложке зададим id, чтобы не влиять на другие элементы с классом overlay*/
   var modalButtons = document.querySelectorAll('.js-open-modal'),
     overlay = document.querySelector('.js-overlay-modal'),
     closeButtons = document.querySelectorAll('.js-modal-close'),
@@ -38,19 +38,19 @@ document.addEventListener('DOMContentLoaded', function () {
     /* Назначаем каждой кнопке обработчик клика */
     item.addEventListener('click', function (e) {
       /* Предотвращаем стандартное действие элемента. Так как кнопку разные
-            люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
-            Нужно подстраховаться. */
+             люди могут сделать по-разному. Кто-то сделает ссылку, кто-то кнопку.
+             Нужно подстраховаться. */
       e.preventDefault();
 
       /* При каждом клике на кнопку мы будем забирать содержимое атрибута data-modal
-            и будем искать модальное окно с таким же атрибутом. */
+             и будем искать модальное окно с таким же атрибутом. */
       var modalId = this.getAttribute('data-modal'),
         modalElem = document.querySelector(
           '.modal[data-modal="' + modalId + '"]'
         );
 
       /* После того как нашли нужное модальное окно, добавим классы
-            подложке и окну чтобы показать их. */
+             подложке и окну чтобы показать их. */
       modalElem.classList.add('active');
       overlay.classList.add('active');
       body.classList.add('modal-open');
@@ -63,6 +63,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
       parentModal.classList.remove('active');
       overlay.classList.remove('active');
+      document.querySelector('body').classList.remove('modal-open');
     });
   }); // end foreach
 
@@ -73,7 +74,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
       if (key == 27) {
         document.querySelector('.modal.active').classList.remove('active');
-        document.querySelector('.overlay').classList.remove('active');
+        overlay.classList.remove('active');
+        document.querySelector('body').classList.remove('modal-open');
       }
     },
     false
@@ -81,6 +83,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
   overlay.addEventListener('click', function () {
     document.querySelector('.modal.active').classList.remove('active');
+    document.querySelector('body').classList.remove('modal-open');
     this.classList.remove('active');
   });
 }); // end ready
